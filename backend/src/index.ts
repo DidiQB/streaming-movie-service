e2e56fc0cb7db3d6ec2d22af59e01115c288c8af
Quine Clarinda <didiq@Quines-MacBook-Pro.local>
2023-04-20 14:21:42 +0200

    connection frontend and backend established

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,30 +15,30 @@ app.use(cors());
 app.use(json());
 
 // Create Connection to MongoDB
-// mongoose.connect(`mongodb+srv://coderaiders5:${process.env.MA_PASSWORD}@coderaiders.9tgceeu.mongodb.net/Streaming?retryWrites=true&w=majority`)
-//     .then(() => console.log('Connected to MongoDB!'));
+mongoose.connect(`mongodb+srv://coderaiders5:${process.env.MA_PASSWORD}@coderaiders.9tgceeu.mongodb.net/Streaming?retryWrites=true&w=majority`)
+    .then(() => console.log('Connected to MongoDB!'));
 
-// app.get
+app.get
 
-// app.post('/movie', async (req, res) => {
-//     // console.log(req.body);
-//     // res.send(req.body);
-//     try {
-//         const movie = await Movie.create(req.body);
-//         res.status(200).json(movie);
-//     } catch (error: any) {
-//         console.log(error.message);
-//         res.status(500).json({ message: error.message });
-//     }
-// });
+app.post('/movie', async (req, res) => {
+    // console.log(req.body);
+    // res.send(req.body);
+    try {
+        const movie = await Movie.create(req.body);
+        res.status(200).json(movie);
+    } catch (error: any) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
 
-// // app.delete
-// // app.put
-// app.use("*", (req, res) => {
-//     res.status(400).json({
-//         message: "Trying to find easter eggs? There are only 3 end points!",
-//     });
-// });
+// app.delete
+// app.put
+app.use("*", (req, res) => {
+    res.status(400).json({
+        message: "Trying to find easter eggs? There are only 3 end points!",
+    });
+});
 
 app.listen(PORT, () =>
     console.log(`Remember to take breaks. Running on localhost:${PORT}`)
--- frontend/src/components/Fetch.tsx
+++ frontend/src/components/Fetch.tsx
@@ -1,7 +1,8 @@
 import {useState, useEffect} from 'react';
 
 const Fetch = () => {
-    const [movie, setMovie] = useState ("") 
+    const [movie, setMovie] = useState ([]);
+    const [savedMovie, setSavedMovie] = useState([]); 
 
     const getMovie = () => {
 
@@ -26,14 +27,43 @@ const Fetch = () => {
           
           fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
           .then(response => response.json())
-          .then(response => console.log(response))
+          .then(response => {
+            console.log(response[1])
+            setMovie(response[1])
+          })
           .catch(err => console.error(err));
           
     }
 
+    const handleSave = async () => {
+      setSavedMovie(movie);
+      console.log(movie);
+      
+      try {
+          const response = await fetch("http://127.0.0.1:3001/movie", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+              },
+              body: JSON.stringify(movie),
+          });
+      
+          if (!response.ok) {
+              throw new Error("Failed to save movie");
+          }
+      
+          // handle response if needed
+      } catch (error) {
+          // handle error if needed
+          throw new Error("Something went wrong when posting to /movie");
+      }
+
+  }  
+
     return (
         <div>
-            <button onChange={getMovie}>Fetch movie</button>
+          <button onClick={handleSave}>Handle Save</button>
+            <button onClick={getMovie}>Fetch movie</button>
         </div>
       );
 }
