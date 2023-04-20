9e6f6d6ef01ef137853fdbc8cd957897b6a0fce8
Quine Clarinda <didiq@Quines-MacBook-Pro.local>
2023-04-20 11:17:07 +0200

    established connection with mongoDB + post
    th branch env + mongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { json } from "express";
+import { v4 as uuidv4 } from "uuid";
+import mongoose from 'mongoose';
+import { Movie } from "./models/movieModels";
 
 dotenv.config();
 
@@ -11,8 +14,24 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(json());
 
+// Create Connection to MongoDB
+mongoose.connect(`mongodb+srv://coderaiders5:${process.env.MA_PASSWORD}@coderaiders.9tgceeu.mongodb.net/Streaming?retryWrites=true&w=majority`)
+  .then(() => console.log('Connected to MongoDB!'));
+
 // app.get
-// app.post
+
+app.post('/movie', async(req, res) => {
+    // console.log(req.body);
+    // res.send(req.body);
+    try {
+      const movie = await Movie.create(req.body);
+      res.status(200).json(movie);  
+    } catch (error:any) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
+
 // app.delete
 // app.put
 app.use("*", (req, res) => {
---
+++ backend/src/models/movieModels.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        rating: {
+            type: String,
+            required: true
+        },
+        year: {
+            type: Number,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        trailer: {
+            type: String,
+            required: true
+        },
+        imdbid: {
+            type: String,
+            required: true
+        }
+    }
+)
+
+export const Movie = mongoose.model('Movies', movieSchema);
